0ad68994491da59346b959b92162bae9ff7aab77
Serhii Antonchyk <antonchyk68@gmail.com>
2019-03-21 15:31:06 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      goods: []
+    };
+  }
+  componentDidMount() {
+    fetch("https://demo3907346.mockable.io/products")
+      .then(res => res.json())
+      .then(result => {
+        this.setState({
+          isLoaded: true,
+          goods: result.products
+        });
+      });
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    const { goods, isLoaded } = this.state;
+    if (!isLoaded) {
+      return <p>Loading...</p>;
+    } else {
+      return (
+        <ul>
+          {goods.map((item, index) => (
+            <li key={index}>{item.name}</li>
+          ))}
+        </ul>
+      );
+    }
   }
 }
 
